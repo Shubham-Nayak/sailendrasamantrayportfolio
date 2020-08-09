9b14feb634c0a201e54d7b63872e2d60dea96c3d
Shubham Nayak <kabirn40@gmail.com>
2020-08-09 20:40:46 +0530

    ssshhh

--- accounts/models.py
+++ accounts/models.py
@@ -45,7 +45,7 @@ class Blog(models.Model):
     id=models.AutoField(primary_key=True)
     title=models.CharField(max_length=25400,default="")
     description=models.CharField(max_length=25000,default="")   
-    createdon=models.CharField(max_length=25400,default="") 
+    createdon=models.CharField(null=True, blank=True) 
 
     def __str__(self):
         class Meta:
